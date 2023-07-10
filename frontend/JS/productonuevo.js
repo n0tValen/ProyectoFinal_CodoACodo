b2755d3f8b6c496e71d67122c343c25656ce4187
Valent <valengonzalez2015.vg@gmail.com>
2023-07-10 14:10:54 -0300

    cambios de ruta y mejoras en codigo

---
+++ frontend/JS/productonuevo.js
@@ -0,0 +1,33 @@
+function guardar() {
+    let n = document.getElementById("nombre").value
+    let p = parseFloat(document.getElementById("precio").value)
+    let s = parseInt(document.getElementById("stock").value)
+    let i = document.getElementById("imagen").value
+
+
+    let producto = {
+        nombre: n,
+        precio: p,
+        stock: s,
+        imagen: i
+    }
+    let url = "mysql+pymsql://root:root@localhost/empleados"
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("creado")
+            alert("Grabado")
+            // Devuelve el href (URL) de la página actual
+            window.location.href = "http://127.0.0.1:5500/frontend/templates/producto.html";  
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            alert("Error al grabar" )
+            console.error(err);
+        })
+}
